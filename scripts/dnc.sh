c3559d296f97426a087fb91bab71f2af98900f29
Guy Waldman <6546430+guywaldman@users.noreply.github.com>
2024-07-22 00:45:49 +0300

    Add DNC script to CI

---
+++ scripts/dnc.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This script is intended to check if there are no "DNC" ("Do Not Commit") comments in the codebase.
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+ROOT_DIR=$(realpath $SCRIPT_DIR/..)
+
+files_to_check=$(git ls-files)
+
+for file in $files_to_check; do
+	file_path="$ROOT_DIR/$file"
+	bad_command=$(rg -C3 "DNC(\(.*\))*:?\s" $file_path)
+	if [ -n "$bad_command" ]; then
+		echo "Found DNC comment!"
+		echo
+
+		echo "FILE PATH:"
+		echo $file_path
+
+		echo "MATCH:"
+		echo "$bad_command"
+		exit 1
+	fi
+done
