48911da1e8085a1e67b93c0bef59ad1d84796da8
Guy Waldman <6546430+guywaldman@users.noreply.github.com>
2024-07-23 07:46:38 +0300

    Add regression test

--- tests/test_suggest.py
+++ tests/test_suggest.py
@@ -1,26 +1,44 @@
 import tempfile
-from tests.utils import run_subcommand, set_config
+
+import pytest
+from tests.utils import Env, run_subcommand, set_config
 
 
 class TestSuggestSubcommand:
-    # NOTE: Skipping this test for now since SLMs provide a lot of flaky tests.
+    def test_basic_openai(self):
+        env = Env.from_env()
+
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp:
+            with open(tmp.name, "w") as f:
+                f.write('{"llm": "openai"}')
+            set_config("llm", "openai", config_path=tmp.name)
+            set_config("openai.model", "gpt-4o-mini", config_path=tmp.name)
+            set_config("openai.api_key", env.openai_api_key, config_path=tmp.name)
+
+            results = run_subcommand(
+                "suggest", ["'Print the current directory using ls. Use only ls'", "--output-only"], mcli_args=["--config", tmp.name]
+            )
+            assert results.status == 0
+            assert "ls" in results.stdout
+
     # TODO: Test with larger foundation models.
-    # def test_basic_ollama(self):
-    #     with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp:
-    #         with open(tmp.name, "w") as f:
-    #             f.write("{}")
+    @pytest.mark.skip(reason="Skipping this test for now since SLMs provide a lot of flaky tests.")
+    def test_basic_ollama(self):
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp:
+            with open(tmp.name, "w") as f:
+                f.write("{}")
 
-    #         set_config("llm", "ollama", config_path=tmp.name)
-    #         set_config("ollama.model", "llama3", config_path=tmp.name)
+            set_config("llm", "ollama", config_path=tmp.name)
+            set_config("ollama.model", "llama3", config_path=tmp.name)
 
-    #         with open(tmp.name, "r") as f:
-    #             print(f.read())
+            with open(tmp.name, "r") as f:
+                print(f.read())
 
-    #         results = run_subcommand(
-    #             "suggest", ["'Print the current directory using `ls`. Use only `ls`'", "--output-only"], mcli_args=["--config", tmp.name]
-    #         )
-    #         assert results.status == 0, "Suggest subcommand failed with status code {}".format(results.status)
-    #         assert "ls" in results.stdout, "Suggest subcommand did not return the expected command"
+            results = run_subcommand(
+                "suggest", ["'Print the current directory using `ls`. Use only `ls`'", "--output-only"], mcli_args=["--config", tmp.name]
+            )
+            assert results.status == 0, "Suggest subcommand failed with status code {}".format(results.status)
+            assert "ls" in results.stdout, "Suggest subcommand did not return the expected command"
 
     def test_fail_if_llm_misconfigured(self):
         with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp:
